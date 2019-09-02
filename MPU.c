862ff9f6a4f30565ab2d9da6fcc089bf15646bb2
Andrew Shen <shenandrew95@gmail.com>
2019-09-02 14:34:31 -0400

    but this does work for mpu

--- MPU.c
+++ MPU.c
@@ -17,24 +17,24 @@ void MPU_setup() {
                                 // Note: Bug with 32 Bytes Protection
                                 // ---
     MPU->RNR = 2;               // Select region 2, Enable User Stack + Run Prog
-    MPU->RBAR = 0x20004000;     // Base Address = 0x20004000 - 0x20005000
-    MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1
-                                // ---
-    MPU->RNR = 2;               // Select region 2, Enable Run Prog
-    MPU->RBAR = 0x20005000;     // Base Address = 0x20005000 - 0x20006000
-    MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 16KB, Enable=1
-                                // ---
-    MPU->RNR = 2;               // Select region 2, Enable Prog Slot 1
-    MPU->RBAR = 0x20006000;     // Base Address = 0x20006000 - 0x20007000
-    MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
-                                // ---
-    MPU->RNR = 2;               // Select region 2, Enable Prog Slot 2
-    MPU->RBAR = 0x20007000;     // Base Address = 0x20007000 - 0x20008000
-    MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
-                                // ----
-    MPU->RNR = 2;               // Select region 2, Enable Prog Slot 3
-    MPU->RBAR = 0x20008000;     // Base Address = 0x20008000 - 0x20009000
-    MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
+    MPU->RBAR = 0x20004000;     // Base Address = 0x20004000 - 0x20006000
+    MPU->RASR = 0x0300001B;     // All Access, TEX=0,S=0,C=0,B=0, 8KB, Enable=1
+    //                             // ---
+    // MPU->RNR = 2;               // Select region 2, Enable Run Prog
+    // MPU->RBAR = 0x20005000;     // Base Address = 0x20005000 - 0x20006000
+    // MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1
+    //                             // ---
+    // MPU->RNR = 2;               // Select region 2, Enable Prog Slot 1
+    // MPU->RBAR = 0x20006000;     // Base Address = 0x20006000 - 0x20007000
+    // MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
+    //                             // ---
+    // MPU->RNR = 2;               // Select region 2, Enable Prog Slot 2
+    // MPU->RBAR = 0x20007000;     // Base Address = 0x20007000 - 0x20008000
+    // MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
+    //                             // ----
+    // MPU->RNR = 2;               // Select region 2, Enable Prog Slot 3
+    // MPU->RBAR = 0x20008000;     // Base Address = 0x20008000 - 0x20009000
+    // MPU->RASR = 0x03000017;     // All Access, TEX=0,S=0,C=0,B=0, 4KB, Enable=1 
     
 
 
